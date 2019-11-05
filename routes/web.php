168bba3efe3f8b89ba8eedacfaccbc0f6df98796
vuthy chann <channvuthy.plan.b@gmail.com>
2019-11-05 10:16:37 +0700

    Middleware/UserAuth.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,13 @@ Route::post('register', 'Update\RegisterController@postRegister');
 
 Route::get('user/verify-account/{code}', 'Update\RegisterController@getVerify');
 
-Route::get('profile', 'Update\UserController@getProfile');
+Route::get('profile', 'Update\UserController@getProfile')->middleware('UserAuth');
 
 Route::get('logout', 'Update\UserController@getLogout');
 
-Route::get('update-profile', 'Update\UserController@getUpdateProfile');
+Route::get('update-profile', 'Update\UserController@getUpdateProfile')->middleware('UserAuth');
 
-Route::get('post', 'Update\UserController@getPost');
+Route::get('post', 'Update\UserController@getPost')->middleware('UserAuth');
 
 Route::post('login', 'Update\UserController@postLogin');
 
@@ -23,17 +23,17 @@ Route::get('detail/{id}', 'Update\ProductController@getDetail');
 
 Route::get('filter', 'Update\ProductController@getFilter');
 
-Route::post('create-ads', 'Update\UserController@postCreatePost');
+Route::post('create-ads', 'Update\UserController@postCreatePost')->middleware('UserAuth');
 
-Route::get('product/user/delete/{id}', 'PostController@getProductUserById');
+Route::get('product/user/delete/{id}', 'PostController@getProductUserById')->middleware('UserAuth');
 
-Route::get('update/product/{id}', 'Update\UserController@getUpdateProduct');
+Route::get('update/product/{id}', 'Update\UserController@getUpdateProduct')->middleware('UserAuth');
 
-Route::get('delete/product/image', 'Update\ProductController@getDeleteProImage');
+Route::get('delete/product/image', 'Update\ProductController@getDeleteProImage')->middleware('UserAuth');
 
-Route::post('update-ads', 'Update\UserController@postUpdateProduct');
+Route::post('update-ads', 'Update\UserController@postUpdateProduct')->middleware('UserAuth');
 
-Route::post('update-profile', 'Update\UserController@postUpdateProfile');
+Route::post('update-profile', 'Update\UserController@postUpdateProfile')->middleware('UserAuth');
 
 Route::post('forgot', 'Update\UserController@postForgot');
 
